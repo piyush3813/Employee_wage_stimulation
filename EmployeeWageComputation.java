780db9b6096d14a39ca097c81f6cb70c41858e11
Piyush Mishra <piyushmishra.p@gmail.com>
2022-04-15 00:14:12 +0530

    added: switch case to check for full time or half time and alloting working hrs

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,17 +5,32 @@ import org.apache.log4j.Logger;
 
 public class EmployeeWageComputation {
     static Logger logger = Logger.getLogger(EmployeeWageComputation.class);
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BasicConfigurator.configure();
         logger.info("Welcome To Employee Wage Computation ");
 
-        final int FULL_TIME = 1;
-        int emp_typ=(int) ((Math.random()*100)%2);
-        if (emp_typ==FULL_TIME){
-            logger.info("the employee is Present");
+        final int FULL_TIME = 2;
+        final int PART_TIME = 1;
+        final int WAGE_PER_HRS = 20;
+        int working_hrs = 0;
+        int emp_typ = (int) ((Math.random() * 100) % 3);
+
+        switch (emp_typ) {
+
+            case (FULL_TIME):
+                logger.info("the employee is Present full time ");
+                working_hrs = 8;
+                break;
+
+            case (PART_TIME):
+                logger.info("the employee is Present Part time ");
+                working_hrs = 4;
+                break;
+
+            default:
+                logger.info("the employee is absent full time ");
         }
-        else{
-            logger.info("the employee is Absent");
-        }
+        int wage = working_hrs * WAGE_PER_HRS;
+        logger.info("employee wage per day is - " + wage);
     }
 }
