053ee1532214a1a6849017e9ae3b745db9088b7c
Piyush Mishra <piyushmishra.p@gmail.com>
2022-04-15 00:18:46 +0530

    added: compution till max hrs is reched or max days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 
 public class EmployeeWageComputation {
     static Logger logger = Logger.getLogger(EmployeeWageComputation.class);
+
     public static void main(String[] args) {
         BasicConfigurator.configure();
         logger.info("Welcome To Employee Wage Computation ");
@@ -12,26 +13,45 @@ public class EmployeeWageComputation {
         final int FULL_TIME = 2;
         final int PART_TIME = 1;
         final int WAGE_PER_HRS = 20;
+        final int MAX_WORKING_HRS = 100;
+        final int MAX_WORKING_DAYS = 20;
+        int total_wage=0;
+
         int working_hrs = 0;
-        int emp_typ = (int) ((Math.random() * 100) % 3);
 
-        switch (emp_typ) {
+        for (int day = 1, total_working_hrs = 0; day < MAX_WORKING_DAYS && total_working_hrs < MAX_WORKING_HRS; day++,total_working_hrs+=working_hrs) {
 
-            case (FULL_TIME):
-                logger.info("the employee is Present full time ");
-                working_hrs = 8;
-                break;
+            int emp_typ = (int) ((Math.random() * 100) % 3);
+            switch (emp_typ) {
 
-            case (PART_TIME):
-                logger.info("the employee is Present Part time ");
-                working_hrs = 4;
-                break;
+                case (FULL_TIME):
+                    logger.info("the employee is Present full time ");
+                    working_hrs = 8;
+                    break;
 
-            default:
-                logger.info("the employee is absent full time ");
+                case (PART_TIME):
+                    logger.info("the employee is Present Part time ");
+                    working_hrs = 4;
+                    break;
+
+                default:
+                    logger.info("the employee is absent full time ");
+            }
+            int wage = working_hrs * WAGE_PER_HRS;
+            logger.info("day | working_hrs  | wage | total_working_hrs ");
+            logger.info(day+"       "+ working_hrs+"           "+ wage+"         "+ (total_working_hrs+working_hrs));
+            total_wage+=wage;
+
         }
-        int wage_per_day = working_hrs * WAGE_PER_HRS;
-        logger.info("employee wage per day is - " + wage_per_day);
-        logger.info("employee wage per month with 20 working days - "+wage_per_day *20);
+        logger.info("total wage = "+total_wage);
     }
 }
+
+
+
+
+
+
+
+
+
