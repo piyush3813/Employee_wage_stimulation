1835be289cd765a6833212d95193f52606188f74
Piyush Mishra <piyushmishra.p@gmail.com>
2022-04-15 00:42:22 +0530

    added: refract the code for multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -9,20 +9,19 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         logger.info("Welcome To Employee Wage Computation ");
-        calculatewage();
+        calculatewage("Amazon", 40, 15, 200);
+        calculatewage("Microsoft", 60, 18, 190);
+
     }
 
-    public static void calculatewage(){
+    public static void calculatewage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs){
         final int FULL_TIME = 2;
         final int PART_TIME = 1;
-        final int WAGE_PER_HRS = 20;
-        final int MAX_WORKING_HRS = 100;
-        final int MAX_WORKING_DAYS = 20;
         int total_wage=0;
 
         int working_hrs = 0;
 
-        for (int day = 1, total_working_hrs = 0; day < MAX_WORKING_DAYS && total_working_hrs < MAX_WORKING_HRS; day++,total_working_hrs+=working_hrs) {
+        for (int day = 1, total_working_hrs = 0; day < maxWorkingDays && total_working_hrs < maxWorkingHrs; day++,total_working_hrs+=working_hrs) {
 
             int emp_typ = (int) ((Math.random() * 100) % 3);
             switch (emp_typ) {
@@ -40,13 +39,14 @@ public class EmployeeWageComputation {
                 default:
                     logger.info("the employee is absent full time ");
             }
-            int wage = working_hrs * WAGE_PER_HRS;
+            int wage = working_hrs * wagePerHr;
             logger.info("day | working_hrs  | wage | total_working_hrs ");
             logger.info(day+"       "+ working_hrs+"           "+ wage+"         "+ (total_working_hrs+working_hrs));
             total_wage+=wage;
 
         }
-        logger.info("total wage = "+total_wage);
+
+        logger.info("Total wage for a month of " + companyName + " employee is " + total_wage + "\n");
     }
 
 }
